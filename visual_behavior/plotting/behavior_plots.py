d4405c1d6b2ffae310e774e34532368eb026cf32
Kate Roll <kater@alleninstitute.org>
2022-03-14 23:04:11 -0700

    lint

--- visual_behavior/plotting/behavior_plots.py
+++ visual_behavior/plotting/behavior_plots.py
@@ -2,26 +2,22 @@
 import os
 import path
 import sys
+import numpy as np
+import matplotlib.pyplot as plt
 
 from .plot_utils import *
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-import data_access as data
-
-import numpy as np
-import matplotlib.pyplot as plt
-
-# from .plot_utils import * 
-# import visual_behavior.data_access as data
+import data_access as data  # noqa: E402
 
 
 def plot_behavioral_streams(dataObject):
     """ plot behavioral streams including running,
         licks, rewards, and df/f streams.
-        
+
     Parameters
     ----------
-        dataObject : (BehaviorSesson, BehaviorOphysExperiment) 
+        dataObject : (BehaviorSesson, BehaviorOphysExperiment)
             Objects provided via allensdk.brain_observatory
             module
     Returns
@@ -59,10 +55,10 @@ def plot_behavioral_streams(dataObject):
 def plot_running(dataObject, ax=None):
     """ plot running speed for trial on specified dataset
     x axis is time (sec), y is running speed (cm/sec)
-        
+
     Parameters
     ----------
-        dataObject : (BehaviorSesson, BehaviorOphysExperiment) 
+        dataObject : (BehaviorSesson, BehaviorOphysExperiment)
             Objects provided via allensdk.brain_observatory
             module
         ax : (matplotlib.axes), optional
@@ -74,12 +70,9 @@ def plot_running(dataObject, ax=None):
     if ax is None:
         fig, ax = plt.subplots()
         ax.set_xlabel("time (sec)")
-
-
     speed, timestamps = data.get_running_speed_timeseries(dataObject)
-
     ax.plot(timestamps, speed,
-        color = DATASTREAM_STYLE_DICT['running_speed']['color'])
+            color=DATASTREAM_STYLE_DICT['running_speed']['color'])  # noqa: F405, E501
     ax.set_title("running speed")
     ax.set_ylabel("speed (cm/s)")
     return ax
@@ -88,10 +81,10 @@ def plot_running(dataObject, ax=None):
 def plot_licks(dataObject, ax=None):
     """ plot licks as tick marks on specified dataset.
     x axis is time (sec) and y axis is lick ticks
-        
+
     Parameters
     ----------
-        dataObject : (BehaviorSesson, BehaviorOphysExperiment) 
+        dataObject : (BehaviorSesson, BehaviorOphysExperiment)
             Objects provided via allensdk.brain_observatory module
         ax : (matplotlib.axes), optional
             Figure axes
@@ -104,24 +97,24 @@ def plot_licks(dataObject, ax=None):
 
     licks = data.get_lick_timestamps(dataObject)
     ax.plot(licks, np.zeros_like(licks), marker="|",
-            linestyle="none", 
-            color = DATASTREAM_STYLE_DICT['licks']['color'])
+            linestyle="none",
+            color=DATASTREAM_STYLE_DICT['licks']['color'])  # noqa: F405
 
 
 def plot_rewards(dataObject, ax=None, reward_type="all"):
     """ plot rewards as blue dots on specified axis
     x axis is time (sec) and y axis is rewards.
-        
+
     Parameters
     ----------
-        dataObject : (BehaviorSesson, BehaviorOphysExperiment) 
+        dataObject : (BehaviorSesson, BehaviorOphysExperiment)
             Objects provided via allensdk.brain_observatory module
         ax : (matplotlib.axes), optional
             Figure axes
         reward_type : string
-            options: 
+            options:
                 "all": all rewards (auto and earned)
-                "auto": only free or unearned rewards 
+                "auto": only free or unearned rewards
                 "earned": only earned (hit on a go trial) rewards
     Returns
     ----------
@@ -131,24 +124,24 @@ def plot_rewards(dataObject, ax=None, reward_type="all"):
         fig, ax = plt.subplots()
 
     reward_timesestamps = data.get_reward_timestamps(dataObject,
-        reward_type=reward_type)
+                                                     reward_type=reward_type)
     ax.plot(
-        reward_timesestamps,
-        np.zeros_like(reward_timesestamps),
-        marker="o",
-        linestyle = "none",
-        color = 'b',
-        markersize = 6,
-        alpha = 0.25,
+            reward_timesestamps,
+            np.zeros_like(reward_timesestamps),
+            marker="o",
+            linestyle="none",
+            color='b',
+            markersize=6,
+            alpha=0.25,
     )
 
 
 def plot_stimuli(dataObject, ax):
     """ plot stimuli as colored bars on specified axis
-        
+
     Parameters
     ----------
-        dataObject : (BehaviorSesson, BehaviorOphysExperiment) 
+        dataObject : (BehaviorSesson, BehaviorOphysExperiment)
             Objects provided via allensdk.brain_observatory
             module
         ax : (matplotlib.axes), optional
@@ -159,17 +152,17 @@ def plot_stimuli(dataObject, ax):
     """
     for _, stimulus in dataObject.stimulus_presentations.iterrows():
         ax.axvspan(stimulus["start_time"],
-        stimulus["stop_time"], alpha=0.5)
+                   stimulus["stop_time"], alpha=0.5)
 
 
 def plot_pupil_area(dataObject, ax=None):
-    """ plot pupil area timeseries. 
+    """ plot pupil area timeseries.
     x axis is time (sec) and y axis is pupil area
-    in pixels squared. 
-        
+    in pixels squared.
+
     Parameters
     ----------
-        dataObject : (BehaviorOphysExperiment) 
+        dataObject : (BehaviorOphysExperiment)
             Object provided via allensdk.brain_observatory
             module
         ax : (matplotlib.axes), optional
@@ -182,68 +175,16 @@ def plot_pupil_area(dataObject, ax=None):
         fig, ax = plt.subplots()
         ax.set_xlabel("time (sec)")
 
-    pupil_area, timestamps = \
-    data.get_pupil_area_timeseries(dataObject)
+    pupil_area, timestamps = data.get_pupil_area_timeseries(dataObject)  # noqa: E501
 
     ax.plot(
         timestamps, pupil_area,
-        color = DATASTREAM_STYLE_DICT['pupil_area']['color'],
+        color=DATASTREAM_STYLE_DICT['pupil_area']['color'],  # noqa: F405
     )
-    ax.set_title(DATASTREAM_STYLE_DICT['pupil_area']['label'])
+    ax.set_title(DATASTREAM_STYLE_DICT['pupil_area']['label'])  # noqa: F405
     ax.set_ylabel("pupil area\n$(pixels^2)$")
     return ax
-    
 
-
-def plot_lick_raster(dataObject, exclude_aborted=False, ax=None):
-
-    """plots distribution of licks across multiple trials
-    Parameters
-    ----------
-        dataObject : (BehaviorSesson, BehaviorOphysExperiment) 
-            Objects provided via allensdk.brain_observatory
-            module
-        exclude_aborted : Boolean
-                Flag to exclude aborted trials, or trials
-                where the animal licks prematurely
-        ax : (matplotlib.axes), optional
-            Figure axes
-    Returns
-    ----------
-    Matplotlib fig,axis
-    """
-    dataObject = data.get_trials_data(dataObject)
-    if exclude_aborted:
-        #subset data to remove aborted trials
-        dataObject = dataObject[dataObject["aborted"] == False]    
-    dataObject = dataObject.reset_index()
-
-    fig,ax = plt.subplots(figsize=(5,10))
-    for trial_index, trial_id in enumerate(dataObject.index.values): 
-        trial_data = dataObject.loc[trial_id]
-        # get times relative to change time
-        lick_times = [(t - trial_data["change_time"]) for t in trial_data["lick_times"]]
-        reward_time = [(t - trial_data["change_time"]) for t in [trial_data["reward_time"]]]
-
-        # plot reward times
-        if len(reward_time)>0:
-               ax.plot(reward_time[0], trial_index + 0.5, 
-                    '.', color='b', label='reward', markersize = 6)
-
-        # plot lick times
-        ax.vlines(lick_times, trial_index, 
-                  trial_index + 1, color='k', linewidth=1)
-
-        # put a line at the change time
-        ax.vlines(0, trial_index, trial_index + 1, 
-                  color=[.5, .5, .5], linewidth = 1)
-
-    # gray bar for response window
-    ax.axvspan(0.1, 0.7, facecolor = 'gray', alpha = .3, edgecolor = 'none')
-    ax.grid(False)
-
-    fig.tight_layout()
-    return fig, ax
 
 if __name__ == "__main__":
     # make_plots(experiment_dataset) - test
