14c15f0a32aef830c16fac7654256a6902cbdd74
Kate R <kater@alleninstitute.org>
2022-03-17 17:17:29 -0700

    update setup.py v 0.1.7

--- setup.py
+++ setup.py
@@ -1,24 +1,12 @@
 from setuptools import setup, find_packages
 
-setup(name='visual_behavior_plotting',
-      version='0.1.6',
-      packages=find_packages(),
-      include_package_data = True,
+setup(name='vb_plotting',
+      version='0.1.7',
       description='Utilities for loading and visualizing data from the Allen Institute Mindscope Visual Behavior Project',  # noqa: E501
       url='https://github.com/DowntonCrabby/visual_behavior_plotting',
       author='University of Washington, MSDS Data515 NeuroViz project group',
       author_email='kater@alleninstitute.org, Braj1@uw.edu, amrit.bhat786@gmail.com',  # noqa: E501
       license='Allen Institute',
-      install_requires=[
-          'flake8>=3.9.8',
-          'pytest>=6.2.4',
-          'allensdk>=2.13.3',
-          'pandas>=1.4.1>=1.4.1',
-          'pytest>=6.2.3',
-          'seaborn>=0.11.1',
-          'path>=16.0.0',
-          'numpy>=1.22.0'
-          ],
       classifiers=[
           'Development Status :: 3 - Alpha',
           'Intended Audience :: Science/Research',
@@ -27,4 +15,17 @@ setup(name='visual_behavior_plotting',
           'Natural Language :: English',
           'Programming Language :: Python :: 3.8'
           ],
+      packages=find_packages(),
+      include_package_data = True,
+      install_requires=[
+          'flake8',
+          'pytest',
+          'pathlib',
+          'path',
+          'allensdk>=2.13.3'
+          ],
+      tests_require=['flake8'],
+
 )
+
+
